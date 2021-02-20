6b490c39352f24e30873905274e4dccc0792f672
Shahriar-Nibir <shahriarnibir615@gmail.com>
2021-02-21 01:20:37 +0600

    2nd commmit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=200, null=True)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.group_name
+
+
+class Poll(models.Model):
+    poll_name = models.CharField(max_length=200, null=True)
+    group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return self.poll_name
+
+
+class Option(models.Model):
+    option_name = models.CharField(max_length=200, null=True)
+    counts = models.PositiveIntegerField(default=0)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.option_name+'-'+self.poll.poll_name
